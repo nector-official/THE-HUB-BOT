64cf175164f7ff8d09a2b35ec5a67cdccdd4e424
nectorofficial <ikhumwa40@gmail.com>
2025-08-07 14:32:58 +0300

    Update tts.js

--- nector/nectorgle/tts.js
+++ nector/nectorgle/tts.js
@@ -1,29 +1,23 @@
-import googleTTS from 'google-tts-api';
 import config from '../../config.cjs';
+import axios from 'axios';
+import fs from 'fs';
 
 const ttsCommand = async (m, Matrix) => {
   const command = m.body.startsWith(config.PREFIX)
     ? m.body.slice(config.PREFIX.length).split(' ')[0].toLowerCase()
     : '';
-
   const args = m.body.slice(config.PREFIX.length + command.length).trim();
 
-  if (!['tts', 'say'].includes(command)) return;
+  if (command !== 'tts') return;
+
+  await Matrix.sendMessage(m.from, { react: { text: "🗣", key: m.key } });
+
+  if (!args) return m.reply('❗ *Enter the text to speak.*');
 
   try {
-    await Matrix.sendMessage(m.from, { react: { text: "🗣", key: m.key } });
+    const url = `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(args)}&tl=en&client=tw-ob`;
 
-    if (!args) {
-      return m.reply("❗ Please provide some text to convert to speech.\n\nExample: `*tts hello how are you`");
-    }
-
-    const url = googleTTS.getAudioUrl(args, {
-      lang: 'hi-IN',
-      slow: false,
-      host: 'https://translate.google.com',
-    });
-
-    await Matrix.sendMessage(m.from, {
+    await Matrix.sendMessage(m.chat, {
       audio: { url },
       mimetype: 'audio/mp4',
       ptt: true
@@ -31,9 +25,8 @@ const ttsCommand = async (m, Matrix) => {
 
   } catch (err) {
     console.error('[TTS Error]', err.message);
-    m.reply("❌ An error occurred while converting text to speech.");
+    m.reply('❌ *Could not generate speech audio.*');
   }
 };
 
 export default ttsCommand;
-  
